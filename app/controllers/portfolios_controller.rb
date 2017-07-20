cd84267528cdb7315d43cb43d9d9471ee7f5f9d8
Jordan Hudgens <jordan@devcamp.com>
2017-07-20 16:43:30 +0000

    Integrated automatic functioanlity within creation and editing of portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,30 +1,31 @@
 class PortfoliosController < ApplicationController
   def index
-     @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.all
   end
-  
+
   def new
-     @portfolio_item = Portfolio.new
+    @portfolio_item = Portfolio.new
   end
-  
-   def create
+
+  def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Successfully created portfolio.' }
+        format.html { redirect_to portfolios_path, notice: 'Successfully created' }
       else
         format.html { render :new }
       end
     end
   end
-  
+
   def edit
     @portfolio_item = Portfolio.find(params[:id])
   end
-  
+
   def update
     @portfolio_item = Portfolio.find(params[:id])
+    
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'Successfully updated.' }
@@ -33,4 +34,5 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
 end
