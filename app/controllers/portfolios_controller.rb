43f051c4c90e1a9369d5691b66c70ab50290a230
Jordan Hudgens <jordan@devcamp.com>
2017-07-26 17:21:51 +0000

    Integrated remove function within portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,5 +38,18 @@ class PortfoliosController < ApplicationController
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
+  
+  def destroy
+    # Performs the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    # Destroys the portfolio
+    @portfolio_item.destroy
+    
+    # Redirects
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Successfully removed.' }
+    end
+  end 
 
 end
